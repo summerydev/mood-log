9bc3822bd29ceac18eda250ff63c5ca5ec312d64
dorimoribebe <dorimori_bebe@naver.com>
2022-04-12 15:26:48 +0900

    ✨

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -4,34 +4,22 @@ import useFetch from "../hooks/useFetch";
 
 export default function Upload() {
   const url: string = "http://localhost:3001/data";
-  const data = useFetch(url);
 
-  const [image, setImage] = useState([]);
-
-  function handleSubmit(e: any) {
-    e.preventDefault();
-    console.log(image);
-    //handleAddItem({ image });
-
-    return (
-      <div className="upload">
-        <span>사진 업로드</span>
-        <form onSubmit={handleSubmit}>
-          <input
-            id=""
-            type="text"
-            required
-            value={image}
-            //onChange={(e) => setImage(e.target.value)}
-            placeholder="url"
-          />
-          <div className="button">
-            <Link type="submit" to="/loading" className="text-link">
-              ai에게 분석맡기기
-            </Link>
-          </div>
-        </form>
+  return (
+    <div className="upload">
+      <span>사진 업로드</span>
+      <input
+        id=""
+        type="text"
+        required
+        //onChange={(e) => setImage(e.target.value)}
+        placeholder="url"
+      />
+      <div className="button">
+        <Link type="submit" to="/loading" className="text-link">
+          ai에게 분석맡기기
+        </Link>
       </div>
-    );
-  }
+    </div>
+  );
 }
