9cc45656c24b5a1810b232fd880f534277410e22
summerydev <summery.dev@gmail.com>
2022-04-16 13:16:11 +0900

    update fetch code

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-//import { useNavigate } from "react-router-dom";
+import { forEachChild } from "typescript";
+//import { useNavigate } from "react-router-dom"; //err
 
 export default function Upload() {
   const [files, setFiles] = useState();
@@ -8,26 +9,33 @@ export default function Upload() {
   const url: string = "http://75da-121-66-139-243.ngrok.io/upload_page";
   const now = Date.now();
 
-  const onLoadFile = (e: any) => {
-    const file = e.target.files[0];
-    console.log(file);
-    setFiles(file);
-  };
+  // const onLoadFile = (e: any) => {
+  //   const file = e.target.files[0];
+  //   console.log(file);
+  //   setFiles(file);
+  // };
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    console.log(e.target);
+    console.log(files);
+    const file = e.target.file.file;
+    let formData = new FormData();
+    formData.append("files", file);
+    const dataSet = {
+      id: now,
+    };
+
+    formData.append("data", JSON.stringify(dataSet));
+
     fetch(url, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "multipart/form-data",
       },
-      body: JSON.stringify({
-        id: now,
-        image: files,
-      }),
+      body: formData,
     });
     //navigate(`/output`);
+    // submit 시 /output으로 넘어가야함
   };
 
   return (
@@ -39,8 +47,8 @@ export default function Upload() {
         <br />
         전신사진일 수록 정확도가 높아진답니다.
       </h3>
-      <form onSubmit={handleSubmit} encType="multipart/form-data">
-        <input id="image" type="file" name="image" onChange={onLoadFile} />
+      <form onSubmit={handleSubmit}>
+        <input id="file" type="file" name="file" />
         <div>
           <button type="submit" className="button">
             ai하두알룩에게 사진 보내기🤖
